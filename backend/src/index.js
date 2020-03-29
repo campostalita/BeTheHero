52b9e2a5d7225ca65484250bdb81ca1677abf28d
campostalita <campos.talita@hotmail.com>
2020-03-29 19:53:05 -0300

    backend final video 2

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const routes = require('./routes')
+const cors = require('cors')
+const routes = require('./routes');
 
 const app = express();
 
+app.use(cors()); // depois mais pra frente coloca o site que entrará
 app.use(express.json());//ele vai até o Insomnia e converte o arquivo em json
 app.use(routes);
 
